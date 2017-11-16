fc3c9b8cad19a5de8ce9bd965333346efe74ebcb
Dan Segal <danSegal2@gmail.com>
2017-11-16 02:07:02 -0500

    Start package for storing fixed length strings

--- src/FixedLengthStrings.jl
+++ src/FixedLengthStrings.jl
@@ -1,5 +1,88 @@
+__precompile__()
+
 module FixedLengthStrings
 
-# package code goes here
+export SString, sstring
+export @tiny_string, @short_string, @mid_string, @long_string, @text_string
+
+using StaticArrays
+
+using Revise
+
+using Compat
+import Compat.String
+
+struct SString{n,T <: Union{Char, UInt8}} <: AbstractString
+    sv::SVector{n,T}
+end
+
+function _string(s::SString)
+    replace(
+        convert(String, s.sv),
+        '\0', ""
+    )
+end
+
+# coercing constructor:
+function sstring(s::String, n=nothing)
+    str_length = length(s)
+
+    ( n == nothing ) && ( n = str_length )
+
+    ( str_length > n ) &&
+        error("String too large to fit into a fixed length of $n")
+
+    nul_length = n - length(s)
+
+    cur_array = eval(parse("b\"$(s)\""))
+
+    cur_nul = eval(parse("$(eltype(cur_array))(C_NULL)"))
+
+    nul_array = repmat([cur_nul], nul_length)
+
+    append!(cur_array, nul_array)
+
+    sv = SVector{n,Char}(cur_array)
+
+    return SString(sv)
+end
+
+sstring(s::AbstractString, n=nothing) = sstring(String(s), n)
+sstring(args...; n=nothing) = sstring(string(args...), n)
+
+macro tiny_string(s, flags...) sstring(s, n=4) end
+macro short_string(s, flags...) sstring(s, n=16) end
+macro mid_string(s, flags...) sstring(s, n=64) end
+macro long_string(s, flags...) sstring(s, n=256) end
+macro text_string(s, flags...) sstring(s, n=1024) end
+
+import Base: write, endof, getindex, sizeof, search, rsearch, isvalid, next, length, IOBuffer, pointer
+
+if isdefined(Base, :bytestring)
+    import Base: bytestring
+    bytestring(s::SString) = bytestring(_string(s))
+end
+
+endof(s::SString) = endof(_string(s))
+next(s::SString, i::Int) = next(_string(s), i)
+length(s::SString) = length(_string(s))
+getindex(s::SString, i::Int) = getindex(_string(s), i)
+getindex(s::SString, i::Integer) = getindex(_string(s), i)
+getindex(s::SString, i::Real) = getindex(_string(s), i)
+getindex(s::SString, i::UnitRange{Int}) = getindex(_string(s), i)
+getindex{T<:Integer}(s::SString, i::UnitRange{T}) = getindex(_string(s), i)
+getindex(s::SString, i::AbstractVector) = getindex(_string(s), i)
+sizeof(s::SString) = sizeof(_string(s))
+search(s::SString, c::Char, i::Integer) = search(_string(s), c, i)
+rsearch(s::SString, c::Char, i::Integer) = rsearch(_string(s), c, i)
+isvalid(s::SString, i::Integer) = isvalid(_string(s), i)
+pointer(s::SString) = pointer(_string(s))
+IOBuffer(s::SString) = IOBuffer(_string(s))
+
+import Base.convert
+const unsafe_convert = Base.convert
+
+@compat unsafe_convert(T::Union{Type{Ptr{UInt8}},Type{Ptr{Int8}}}, s::SString) = convert(T, _string(s))
+unsafe_convert(::Type{Cstring}, s::SString) = unsafe_convert(Cstring, _string(s))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
 using FixedLengthStrings
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+test_string = "foo"
+
+test_tiny = @tiny_string "foo"
+test_short = @short_string "foo"
+test_mid = @mid_string "foo"
+
+@test test_tiny == test_string
+@test test_tiny == test_short
+@test test_tiny == test_mid
+
+tiny_length = 4
+short_length = 16
+mid_length = 64
+
+@test length(test_tiny.sv) == tiny_length
+@test length(test_short.sv) == short_length
+@test length(test_mid.sv) == mid_length
+
+@test_throws ErrorException try @eval @tiny_string("foobar") catch err; throw(err.error) end
